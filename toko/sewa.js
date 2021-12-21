69569c225314e339825c80d0b6d78ce08ca8152b
Ndraa27 <95161935+Ndraa27@users.noreply.github.com>
2021-12-21 08:37:55 +0700

    Update and rename shop/sewa.js to toko/sewa.js

---
+++ toko/sewa.js
@@ -0,0 +1,22 @@
+const pc_sewa = (_minggu, __minggu, ___minggu, _bulan, _permanen, __permanen) => {
+return `「 *PRICE LIST* 」
+							
+⬣ PRICE SEWABOT
+
+⬡ 1 MINGGU = ${10k minggu}
+⬡ 2 MINGGU = ${15k minggu}
+⬡ 1 BULAN = ${20k bulan}
+⬡ PERMANEN = ${25k permanen}
+⬡ PERMANEN + USER PREM = ${30k permanen}
+
+
+⬣ MINAT? PM
+⬡ wa.me/6288211784515
+
+
+NOTE : 
+Bot On 24 Jam Karna Di Run Menggunakan Heroku
+Reset Data Setiap Jam 02:00 `
+	}
+
+exports.pc_sewa = pc_sewa
