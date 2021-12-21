db660e35144994c55897fd0c08c2b685ba4b552f
Ndraa27 <95161935+Ndraa27@users.noreply.github.com>
2021-12-21 08:56:50 +0700

    Update and rename shop/allpayment.js to toko/allpayment.js

---
+++ toko/allpayment.js
@@ -0,0 +1,24 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY INDRALSMNA27
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 0882 1178 4515
+2. OVO
+	ㅁ 0882 1178 4515
+3. DANA
+	ㅁ 0882 1178 4515
+	
+Semuanya atas nama IN*******NA
+
+*📲 PULSA*
+
+Tidak tersedia
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
