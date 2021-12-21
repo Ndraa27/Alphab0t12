4ddc3f58d899f30c10d98d5c6cf8e08512486b51
Ndraa27 <95161935+Ndraa27@users.noreply.github.com>
2021-12-21 08:27:57 +0700

    Update and rename shop/donasi.js to toko/donasi.js

---
+++ toko/donasi.js
@@ -0,0 +1,19 @@
+const donasibot = () => {
+return `DONASI BISA VIA BANK / E-MONEY
+
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 0882 1178 4515
+2. OVO
+	ㅁ 0882 1178 4515
+3. DANA
+	ㅁ 0882 1178 4515 [ Diutamakan ]
+	
+Semuanya atas nama IN********NA
+`
+	}
+
+exports.donasibot = donasibot
+ 
